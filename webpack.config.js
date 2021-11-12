3cc190fcb25b79cd8275ee1078ec81845b2873ca
Liqun Liu <liqun.liu@ec-lyon.fr>
2021-11-12 16:09:05 +0100

    Add the sort drop down, not finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 
-
-
-// const path = require('path');
+// var path = require('path');
 // const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
     // mode: 'none',
     // devtool: 'eval-source-map',
-    // entry: __dirname + "/main.js",
+    // entry: __dirname + "/index",
     // output: {
     //     path: __dirname + "/public",
     //     filename: "bundle.js"
@@ -18,6 +16,7 @@ module.exports = {
     devServer: {
         // publicPath: '/',
         contentBase: __dirname,//本地服务器所加载的页面所在的目录 
+        // contentBase: path.join(__dirname,'index.html'),
         historyApiFallback: true,//不跳转 
         inline: false,//实时刷新 
         port: 3000,
